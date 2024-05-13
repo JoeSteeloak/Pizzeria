3e1f7c24465efdf215380e7668f7936c2eab0e85
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-13 14:52:09 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/* Ladda ner menyn och visa den */
+window.onload = init;
+async function init() {
+    const url = "http://127.0.0.1:3000";
+
+    const response = await response.json();
+    console.log(data);
+};
