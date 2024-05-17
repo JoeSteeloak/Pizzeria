5b4cea2c8fa544df0b930f07d3e61dd889b05b25
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-17 23:10:19 +0200

    add frontend booking functionality

---
+++ src/js/booking.js
@@ -0,0 +1,44 @@
+const url = "http://127.0.0.1:3000/booking" //URL till mitt API
+const bookingFormEl = document.getElementById('bookingForm');
+
+/* funktion för att läsa in datan i formuläret */
+const bookBtnEl = document.getElementById("bookBtn");
+bookBtnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById("booking-name").value;
+    let phonenumber = document.getElementById("booking-phone").value;
+    let numberOfGuests = document.getElementById("booking-amount").value;
+    let date = document.getElementById("booking-date").value;
+
+    createBooking(name, phonenumber, numberOfGuests, date);
+});
+
+/* Funktion för att lägga till bokningar i databasen */
+async function createBooking(name, phonenumber, numberOfGuests, date) {
+    let booking = {
+        name: name,
+        phonenumber: phonenumber,
+        numberOfGuests: numberOfGuests,
+        date: date
+    }
+    let bookingStatus = document.getElementById("bookingStatus");
+
+    const response = await fetch(url + "/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(booking)
+    });
+
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+        bookingStatus.innerHTML = `${data.error}`;
+    } else {
+        bookingStatus.innerHTML = `${data.message} <i class="fa-solid fa-check"></i>`;
+        bookBtnEl.disabled = true;
+        bookBtnEl.classList.add("disabled");
+    }
+}
