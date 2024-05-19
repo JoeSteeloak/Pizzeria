d473fca55fcef355bcd6a67da3394dae27816319
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-19 08:45:48 +0200

    finish message function + add 2 new admin pages

---
+++ src/js/contact.js
@@ -0,0 +1,41 @@
+const url = "http://127.0.0.1:3000/message" //URL till mitt API
+const contactFormEl = document.getElementById('contactForm');
+
+/* funktion för att läsa in datan i formuläret */
+const contactBtnEl = document.getElementById("submit");
+contactBtnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    createMessage(name, email, message);
+});
+
+/* Funktion för att lägga till meddelanden i databasen */
+async function createMessage(name, email, message) {
+    let contact = {
+        name: name,
+        email: email,
+        message: message,
+    };
+
+    const response = await fetch(url + "/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(contact)
+    });
+
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+        console.log(data.error);
+    } else {
+        contactBtnEl.disabled = true;
+        contactBtnEl.value = "Tack för meddelandet!";
+    }
+}
+
