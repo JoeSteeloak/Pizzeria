ffa140dc96b97a2c9a989c26bd882a53aa5870fa
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-19 09:20:20 +0200

    finish admin booking page and functionality

---
+++ src/js/admin-bookings.js
@@ -0,0 +1,73 @@
+// logga ut-knapp
+const logOutBtnEl = document.getElementById("logoutbtn");
+logOutBtnEl.addEventListener("click", logout);
+function logout() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("token");
+    window.location.href = 'index.html';
+}
+
+
+const url = "http://127.0.0.1:3000/booking" //URL till mitt API
+
+/* Hämta bokningarna */
+window.onload = init;
+async function init() {
+    const token = localStorage.getItem('token');
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+    });
+    const data = await response.json();
+    console.log(data);
+    displayBookings(data);
+};
+
+//skriv ut bokningarna
+function displayBookings(data) {
+    const bookingListEl = document.getElementById("bookingList");
+
+    data.forEach(e => {
+        const bookingItem = document.createElement("div");
+        bookingItem.classList= "bookingItem";
+        bookingItem.innerHTML +=
+        `<h4 class="name">Gästens namn: ${e.name}</h4>
+        <p class="phonenumber">Telefonnummer: ${e.phonenumber}</p>
+        <p class="numberOfGuests">Antal gäster: ${e.numberOfGuests}</p> 
+        <p class="date">Datum: ${e.date}</p> 
+        <p class="created">Skapad: ${e.created}</p> 
+        </div>`;
+        const deleteBookingBtn = document.createElement("input");
+        deleteBookingBtn.type = "button";
+        deleteBookingBtn.value = "Radera bokning";
+        deleteBookingBtn.addEventListener('click', () => {
+            deleteBooking(e._id);
+        });
+        bookingListEl.appendChild(bookingItem);
+        bookingItem.appendChild(deleteBookingBtn);
+
+    });
+};
+
+//radera bokingarna
+async function deleteBooking(id) {
+
+    const token = localStorage.getItem('token');
+
+    const response = await fetch(url + "/" + id, {
+        method: "DELETE",
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            "Content-Type": "Application/json"
+        }});
+
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+        /* status.innerHTML = `${data.error}`; */
+    } else {
+        /* status.innerHTML = `${data.message}`; */
+        location.reload()
+    }
+}
